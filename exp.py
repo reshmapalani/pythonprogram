df90d5dd63a27dc05e44d1ef3aa300d1f21ecc72
reshmapalani <48348519+reshmapalani@users.noreply.github.com>
2019-03-11 14:39:27 +0530

    Add files via upload

---
+++ exp.py
@@ -0,0 +1,16 @@
+try:
+	a=int(input("enter the number"))
+	if a<3:
+		b=int(a/(a-3))
+	elif a==3:
+		raise ZeroDivisionError
+	else:
+		raise NameError
+except ZeroDivisionError:
+	print("some error occured")
+except NameError:
+	print("u have choosen a number greater than 3")
+else:
+	print(b)
+
+
